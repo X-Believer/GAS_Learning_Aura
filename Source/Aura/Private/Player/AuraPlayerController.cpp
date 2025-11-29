ebd705ba0f89f1f5f380f8899b0aeaf11d049a1a
Shuyang Xing <shuyang-xing@outlook.com>
2025-11-29 17:33:44 +0800

    Add EnemyInterface

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,68 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+	
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+	
+	/**
+	 * Line trace cursor, there are several scenarios:
+	 * A. LastActor == ThisActor == nullptr, do nothing
+	 *	- Do nothing
+	 * B. LastActor == nullptr, ThisActor is valid:
+	 *	- Highlight actor
+	 * C. LastActor is valid, ThisActor == nullptr:
+	 *  - UnHighlight actor
+	 * D. LastActor is valid, ThisActor is valid, LastActor != ThisActor:
+	 *  - UnHighlight last actor, highlight this actor
+	 * E. LastActor is valid, ThisActor is valid, LastActor == ThisActor:
+	 *  - Do nothing
+	 */
+	
+	if (LastActor == nullptr)
+	{
+		if (ThisActor)
+		{
+			// Case	B
+			ThisActor->HighlightActor();
+		}
+	}
+	else if (ThisActor == nullptr)
+	{
+		if (LastActor)
+		{
+			// Case C
+			LastActor->UnHighlightActor();
+		}
+	}
+	else if (LastActor != ThisActor)
+	{
+		if (LastActor)
+		{
+			// Case D
+			LastActor->UnHighlightActor();
+			ThisActor->HighlightActor();
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
